6d192dddf978e36d4c9e22fa7cf63c9deed6f6bc
Morkmork23 <markjennuelgarcia@gmail.com>
2024-12-04 03:57:58 +0800

    start

--- quizapp/app_main/views.py
+++ quizapp/app_main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello, world!")
+def landing_page(request):
+    return render(request, 'landing_page.html')
