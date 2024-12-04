e80e9d7d9db3c6d42105990d96599231701aec0e
Morkmork23 <markjennuelgarcia@gmail.com>
2024-12-04 13:25:09 +0800

    w landing page example

--- quizapp/app_main/views.py
+++ quizapp/app_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def landing_page(request):
-    return render(request, 'landing_page.html')
+    return render(request, 'Features/landing_page.html')
