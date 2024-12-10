7842b281140fedeb5e8b45d400a15b8642c9ef23
Morkmork23 <markjennuelgarcia@gmail.com>
2024-12-11 01:17:33 +0800

    teachers app and student app

--- quizapp/quizapp/urls.py
+++ quizapp/quizapp/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("app_main.urls")),
+    path("student/", include("student_app.urls")),
+    path("teacher/", include("teacher_app.urls"))
 ]
