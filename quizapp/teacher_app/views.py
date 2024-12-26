cb53081bcd2e921fc0662f1a93379466ae568744
johnasblasco <johnaslblasco@gmail.com>
2024-12-26 15:41:59 +0800

    wait

--- quizapp/teacher_app/views.py
+++ quizapp/teacher_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.urls import reverse
+from django.http import HttpResponse
 
-# quiz_home dashboard
 def dashboard(request):
     return render(request, 'teacher_dashboard.html')
+
+def create_class(request):
+    return HttpResponse("Create Class Page")
+
+def create_quiz(request):
+    return HttpResponse("Create Quiz Page")
+
+def manage_class(request, class_id):
+    return HttpResponse(f"Manage Class Page for Class ID {class_id}")
+
+def view_results(request, quiz_id):
+    return HttpResponse(f"View Results Page for Quiz ID {quiz_id}")
